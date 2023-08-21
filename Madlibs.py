9bde66aa1ede2594eee7ca77f89a22fab1273e47
Faiz TA <faiznkm@gmail.com>
2023-08-21 23:07:49 +0530

    Basic Madlibs created

---
+++ Madlibs.py
@@ -0,0 +1,9 @@
+adjective = input("Adjective: ")
+do = input("What to do: ")
+verb = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adjective}! It makes me so excited all the time because \
+I love to {do}. Stay hydrated and {verb} like you are {famous_person}!"
+
+print(madlib)
